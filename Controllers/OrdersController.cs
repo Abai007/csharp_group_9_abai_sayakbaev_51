0f6ae4c3b2ab6a5b1baaa8493a63a1a7013283fc
Abai Sayakbaev <abaiskii01@gmail.com>
2021-08-19 14:44:28 +0600

    Start project ready

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,40 @@
+﻿using homework_51_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework_51_1.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private MobileContext _db;
+        public OrdersController(MobileContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Order> orders = _db.Orders.Include(o => o.Phone).ToList();
+            return View(orders);
+        }
+        public IActionResult Create(int phoneId)
+        {
+            Phone phone = _db.Phones.FirstOrDefault(p => p.Id == phoneId);
+            return View(new Order { Phone = phone });
+        }
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            if (order != null)
+            {
+                _db.Orders.Add(order);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
